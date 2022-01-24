ca30fc5c17d586777ef69c4241f2cf13756e9920
Tabish Amin <tabishamin@MP17-TabishA-SE-445.local>
2022-01-24 18:32:39 +0500

    New Files Added

---
+++ practice/context.go
@@ -0,0 +1,62 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"runtime"
+// 	"time"
+// )
+
+// func printNumbers() {
+// 	counter := 1
+
+// 	for {
+// 		time.Sleep(100 * time.Millisecond)
+// 		counter++
+// 	}
+// }
+
+// func main() {
+// 	go printNumbers()
+
+// 	fmt.Println("Before: active goroutines", runtime.NumGoroutine())
+// 	time.Sleep(time.Second)
+
+// 	fmt.Println("After: active goroutines", runtime.NumGoroutine())
+// 	fmt.Println("Program exited")
+// }
+
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+var signal = make(chan bool)
+
+func printNumbers() {
+	counter := 1
+
+	for {
+		select {
+		case <-signal:
+			return
+		default:
+			time.Sleep(100 * time.Millisecond)
+			counter++
+		}
+	}
+}
+
+func main() {
+	go printNumbers()
+
+	fmt.Println("Before: active goroutines", runtime.NumGoroutine())
+	time.Sleep(time.Second)
+
+	signal <- true
+
+	fmt.Println("After: active goroutines", runtime.NumGoroutine())
+	fmt.Println("Program exited")
+}
--- practice/go.mod
+++ practice/go.mod
@@ -9,5 +9,7 @@ require (
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/rakyll/gotest v0.0.6 // indirect
+	github.com/thatisuday/clapper v1.0.10 // indirect
+	github.com/thatisuday/commando v1.0.4 // indirect
 	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
 )
